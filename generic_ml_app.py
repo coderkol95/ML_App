d19d406005be40cd5f1e0493821d45175f7d1be0
coderkol95 <innocentangelfromhell@gmail.com>
2022-06-27 15:08:21 +0530

    first_commit

---
+++ generic_ml_app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder, OneHotEncoder
+from sklearn.impute import KNNImputer, IterativeImputer
+
+from sklearn
+
+# TODO-preprocessing
+# 1. Impute missing values
+
+
+
+
+# 2. Scale the data / Encoding
+# 3. Feature selection 
+# 4. Split into train/test
